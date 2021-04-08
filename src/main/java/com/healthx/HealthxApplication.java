51b160fd66427a7a51c858c217eaeb2488c71937
Serhii Rudyi <srudyjj@gmail.com>
2021-04-08 20:08:19 +0300

    1.4

--- src/main/java/com/healthx/HealthxApplication.java
+++ src/main/java/com/healthx/HealthxApplication.java
@@ -1,4 +1,4 @@
-package com.healthx.healthx;
+package com.healthx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/healthx/config/AuthServerConfig.java
@@ -0,0 +1,45 @@
+package com.healthx.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private final AuthenticationManager authenticationManager;
+
+    private final TokenStore tokenStore;
+
+    private final JwtAccessTokenConverter converter;
+
+    public AuthServerConfig(AuthenticationManager authenticationManager,
+                                            TokenStore tokenStore,
+                                            JwtAccessTokenConverter converter) {
+        this.authenticationManager = authenticationManager;
+        this.tokenStore = tokenStore;
+        this.converter = converter;
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(tokenStore)
+                .accessTokenConverter(converter);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client")
+                .secret("secret")
+                .authorizedGrantTypes("authorization_code", "password", "refresh_token")
+                .scopes("read");
+    }
+}
